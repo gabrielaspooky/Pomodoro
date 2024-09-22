465d8a891dd1a15ad99d62f17a0df6718b718aff
gabrielaspooky <gabrielanava01@gmail.com>
2024-09-22 17:29:05 +0200

    redid timerdisplay

--- pomodoro-timer/components/ui/TimerDisplay.jsx
+++ pomodoro-timer/components/ui/TimerDisplay.jsx
@@ -2,135 +2,150 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import { ToastContainer } from 'react-toastify';
+import UserCard from './UserCard';
 import UserJoinedToast from './JoinToast';
 import UserLeftToast from './LeaveToast';  
 import MemoryMatch from './MemoryMatch';
 import Navbar from './Navbar';
-import UserCard from './UserCard';
 import Pom from './Pom';
-
-const imageUrls = [
-  'https://images.pexels.com/photos/5465339/pexels-photo-5465339.jpeg',
-  'https://images.pexels.com/photos/5465502/pexels-photo-5465502.jpeg',
-  'https://images.pexels.com/photos/18111115/pexels-photo-18111115/free-photo-of-pomeranian-standing-on-grass.jpeg',
-  'https://images.pexels.com/photos/11901851/pexels-photo-11901851.jpeg',
-  'https://images.pexels.com/photos/10405999/pexels-photo-10405999.jpeg',
-  'https://images.pexels.com/photos/3887670/pexels-photo-3887670.jpeg',
-  'https://images.pexels.com/photos/5617166/pexels-photo-5617166.jpeg'
-];
-
-const useSocket = (room) => {
-  const [socket, setSocket] = useState(null);
-  const [users, setUsers] = useState({});
-  const [newUser, setNewUser] = useState(null);
-  const [userLeft, setUserLeft] = useState(null);
-
-  useEffect(() => {
-    const socketIo = io('https://socketserver-production-3e3c.up.railway.app');
-    setSocket(socketIo);
-    socketIo.emit('join_room', room);
-
-    socketIo.on('user_joined', (users) => {
-      const userIds = Object.keys(users);
-      const latestUserId = userIds[userIds.length - 1];
-      const randomImage = imageUrls[Math.floor(Math.random() * imageUrls.length)];
-      const newUser = { ...users[latestUserId], image: randomImage };
-      setNewUser(newUser);
-      setUsers((prevUsers) => ({ ...prevUsers, [latestUserId]: newUser }));
-    });
-
-    socketIo.on('user_left', (user) => {
-      setUserLeft(user);
-      setUsers((prevUsers) => {
-        const { [user.id]: _, ...newUsers } = prevUsers;
-        return newUsers;
-      });
-    });
-
-    return () => {
-      socketIo.emit('leave_room', room);
-      socketIo.disconnect();
-    };
-  }, [room]);
-
-  return { socket, users, newUser, userLeft };
-};
 
 const PomodoroTimer = () => {
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
   const [isBreak, setIsBreak] = useState(false);
-  const [cycleCount, setCycleCount] = useState(0);
-  const room = 'your-room-id';
+  const [socket, setSocket] = useState(null);
+  const [room] = useState('your-room-id');
+  const [users, setUsers] = useState({});
+  const [newUser, setNewUser] = useState(null);
+  const [userLeft, setUserLeft] = useState(null);
+  const [cycleCount, setCycleCount] = useState(0); // Nuevo estado para el contador de ciclos
 
-  const { socket, users, newUser, userLeft } = useSocket(room);
+  useEffect(() => {
+    const socketIo = io('https://socketserver-production-3e3c.up.railway.app');
+    setSocket(socketIo);
+
+    // Unirse a la sala
+    socketIo.emit('join_room', room);
+
+    // Cuando un usuario se une
+    socketIo.on('user_joined', (users) => {
+      const userIds = Object.keys(users);
+      const latestUserId = userIds[userIds.length - 1];
+      setNewUser(users[latestUserId]);
+      setUsers(users);
+    });
+
+    // Cuando un usuario se va (por desconexión o botón)
+    socketIo.on('user_left', (user) => {
+      setUserLeft(user);  // Guardamos al usuario que salió
+      setUsers((prevUsers) => {
+        const { [user.id]: _, ...newUsers } = prevUsers;
+        return newUsers;
+      });
+    });
+
+    // Manejar desconexión
+    return () => {
+      socketIo.emit('leave_room', room);
+      socketIo.disconnect();
+    };
+  }, [room]);
+
+  const handleLeaveRoom = () => {
+    socket?.emit('leave_room', room);
+    socket?.disconnect();
+    // Aquí puedes agregar un redireccionamiento o lógica adicional si es necesario
+  };
 
   useEffect(() => {
     let interval = null;
+
     if (isActive) {
-      interval = setInterval(handleTimerUpdate, 1000);
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 0) {
+            if (minutes === 0) {
+              if (!isBreak) {
+                // Cambia a tiempo de descanso
+                setIsBreak(true);
+                setMinutes(5);
+                setCycleCount((prevCycleCount) => prevCycleCount + 1); // Incrementar contador de ciclos
+                return 0;
+              } else {
+                // Vuelve a tiempo de trabajo
+                setIsBreak(false);
+                setMinutes(25);
+                return 0;
+              }
+            } else {
+              setMinutes((prevMinutes) => prevMinutes - 1);
+              return 59;
+            }
+          } else {
+            return prevSeconds - 1;
+          }
+        });
+
+        socket?.emit('update_timer', {
+          minutes,
+          seconds,
+          isActive,
+          isBreak,
+        });
+
+      }, 1000);
     } else {
       clearInterval(interval);
     }
+
     return () => clearInterval(interval);
-  }, [isActive]);
-
-  const handleTimerUpdate = () => {
-    setSeconds((prevSeconds) => {
-      if (prevSeconds === 0) {
-        if (minutes === 0) {
-          handleSessionEnd();
-          return 0;
-        } else {
-          setMinutes((prev) => prev - 1);
-          return 59;
-        }
-      } else {
-        return prevSeconds - 1;
-      }
-    });
-  };
-
-  const handleSessionEnd = () => {
-    if (!isBreak) {
-      setIsBreak(true);
-      setMinutes(5);
-      setCycleCount((prev) => prev + 1);
-    } else {
-      setIsBreak(false);
-      setMinutes(25);
-    }
-  };
+  }, [isActive, minutes, seconds, isBreak, socket]);
 
   const toggleTimer = () => {
-    setIsActive((prev) => !prev);
-    socket?.emit('update_timer', { minutes, seconds, isActive: !isActive, isBreak });
+    setIsActive(!isActive);
+    socket?.emit('update_timer', {
+      minutes,
+      seconds,
+      isActive: !isActive,
+      isBreak,
+    });
   };
 
   const resetTimer = () => {
     setIsActive(false);
-    setMinutes(25);
+    setMinutes(25); // cambia aquí para test
     setSeconds(0);
     setIsBreak(false);
-    setCycleCount(0);
-    socket?.emit('update_timer', { minutes: 25, seconds: 0, isActive: false, isBreak: false });
+    setCycleCount(0); // Reiniciar contador de ciclos
+    socket?.emit('update_timer', {
+      minutes: 25,
+      seconds: 0,
+      isActive: false,
+      isBreak: false,
+    });
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gradient-to-br from-indigo-300 via-purple-300 to-pink-300">
-      <Navbar onLeave={() => socket?.emit('leave_room', room)} />
+      <Navbar onLeave={handleLeaveRoom} />
       <ToastContainer />
       <Pom isBreak={isBreak} />
       {isBreak && <MemoryMatch />}
-      <div className="text-6xl font-mono mb-6 text-white drop-shadow-lg">
+      <div className="text-6xl font-mono mb-6 text-white drop-shadow-lg transition-transform duration-300 transform hover:scale-105">
         {String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}
       </div>
       <div className="flex space-x-4">
-        <button onClick={toggleTimer} className={`bg-${isActive ? 'red' : 'green'}-500 text-white px-6 py-2 rounded-full`}>
+        <button
+          onClick={toggleTimer}
+          className={`bg-${isActive ? 'red' : 'green'}-500 text-white px-6 py-2 rounded-full font-semibold shadow-md hover:bg-${isActive ? 'red' : 'green'}-600 transition-colors duration-200 transform hover:scale-105`}
+        >
           {isActive ? 'Pause' : 'Start'}
         </button>
-        <button onClick={resetTimer} className="bg-gray-500 text-white px-6 py-2 rounded-full">
+        <button
+          onClick={resetTimer}
+          className="bg-gray-500 text-white px-6 py-2 rounded-full font-semibold shadow-md hover:bg-gray-600 transition-colors duration-200 transform hover:scale-105"
+        >
           Reset
         </button>
       </div>
@@ -141,7 +156,7 @@ const PomodoroTimer = () => {
         <h2 className="text-2xl font-bold text-white mb-4">Usuarios en la sala:</h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
           {Object.values(users).map((user) => (
-            <UserCard key={user.id} username={user.username} activity={user.activity} imageUrl={user.image} />
+           <UserCard key={user.id} userId={user.id} activity={user.activity} />
           ))}
         </div>
       </div>
