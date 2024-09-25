7c9a0254adc139d01508c01ef4204feb494e15ba
gabrielaspooky <gabrielanava01@gmail.com>
2024-09-25 20:14:40 +0200

    added new style to memory cards

--- pomodoro-timer/src/app/page.js
+++ pomodoro-timer/src/app/page.js
@@ -42,7 +42,6 @@ const Home = () => {
         <>
           <UserCard username={userData.username} activity={userData.activity} /> 
           <TimerDisplay username={userData.username} activity={userData.activity} />
-          <BreakTimeModal />
           <ApiFetcher />
           <PomodoroFaq />
         </>
